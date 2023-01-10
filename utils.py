bb4fc1a8d743c983b77519b7b5293604eb0cf5f9
Miran <miran.oezdogan@campus.lmu.de>
2023-01-10 02:16:01 +0100

    additional diversity evaluation metrics

--- utils.py
+++ utils.py
@@ -100,6 +100,73 @@ def test(test_loader, model, criterion, regularizer=None, **kwargs):
         'accuracy': correct * 100.0 / len(test_loader.dataset),
     }
 
+def test_extensive(test_loader, model, criterion, regularizer=None, t_refs = [0, 1], **kwargs):
+    """
+    computes additional metrics in the test loop including disagreement rate, the added true rate, and the added true rate compared to an optimal ensemble of the reference models (predictions none of the reference models made true)
+
+    Paramters:
+        test_loader : loads test data
+        model : model to be evaluated
+        criterion : function to return NLL (cross entropy or nll) depending on whether output is logits or probs
+        regularizer : function that takes model and returns penalty term
+        ref_models : List containing models the model is to be compared to #predictions are being made again and again for them - might optimize to store
+    
+    Returns:
+        Dict containing the test metrics
+    """
+
+    
+
+    loss_sum = 0.0
+    nll_sum = 0.0
+    correct = 0.0
+    disagreements = np.array([0.0] * len(t_refs)) #holds amount of samples the model disagrees with for each reference model
+    added_trues = np.array([0.0] * len(t_refs)) #holds amount of true predictions that the reference model failed on
+    added_trues_to_all = 0.0
+
+    model.eval()
+
+    for input, target in test_loader:
+        input = input.cuda(non_blocking =True)
+        target = target.cuda(non_blocking =True)
+
+        output = model(input, **kwargs)
+        ref_outputs = [model(input, t_ref) for t_ref in t_refs]
+
+        nll = criterion(output, target)
+        loss = nll.clone()
+        if regularizer is not None:
+            loss += regularizer(model)
+
+        nll_sum += nll.item() * input.size(0)
+        loss_sum += loss.item() * input.size(0)
+        pred = output.data.argmax(1, keepdim=True)
+        correct_pred = pred.eq(target.data.view_as(pred))
+        correct += correct_pred.sum().item()
+
+        ref_preds = [ref_output.data.argmax(1, keepdim=True) for ref_output in ref_outputs]
+        ref_disagreements = np.array([sum(pred != ref_pred).item() for ref_pred in ref_preds])
+        disagreements += ref_disagreements
+
+        ref_corrects = [ref_pred.eq(target.data.view_as(ref_pred)) for ref_pred in ref_preds]
+        added_trues += np.array([sum(torch.logical_and(ref_correct, ~correct_pred)).item() for ref_correct in ref_corrects])
+
+        ref_corrects = np.array([x.cpu().detach().numpy() for x in ref_corrects])
+        any_ref_correct = np.apply_along_axis(np.any, 0, ref_corrects)
+        correct_pred = correct_pred.cpu().detach().numpy()
+        added_trues_to_all += sum(correct_pred & ~ any_ref_correct)
+
+
+        N = len(test_loader.dataset)
+
+    return {
+        'nll': nll_sum / N,
+        'loss': loss_sum / N,
+        'accuracy': correct * 100.0 / N,
+        "disagreement" : disagreements / N,
+        "new_true" : added_trues / N,
+        "new_true_total" : added_trues_to_all / N
+    }
 
 
 def predictions(test_loader, model, **kwargs):
