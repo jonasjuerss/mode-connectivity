a1b4bfcbd68ac34188d172e8d9df64a613d321c3
Miran <miranoezdogan@gmail.com>
2023-01-16 18:20:12 +0100

    eval tunnel system cross road

---
+++ eval_cross_road.py
@@ -0,0 +1,92 @@
+import argparse
+import numpy as np
+import os
+import tabulate
+import torch
+import torch.nn.functional as F
+
+import data
+import models
+import curves
+import utils
+
+
+def main(args):
+    os.makedirs(args.dir, exist_ok=True)
+
+    torch.backends.cudnn.benchmark = True
+
+    loaders, num_classes = data.loaders(
+        dataset = args.dataset,
+        path = args.data_path,
+        batch_size = args.batch_size,
+        num_workers = args.num_workers,
+        transform_name = args.transform,
+        use_test = args.use_test,
+        shuffle_train=False
+    )
+
+    architecture = getattr(models, args.model)
+    curve = getattr(curves, args.curve)
+    model = curves.CurveNet(
+        num_classes,
+        curve,
+        architecture.curve,
+        args.num_bends,
+        architecture_kwargs=architecture.kwargs,
+    )
+    model.cuda()
+    checkpoint = torch.load(args.ckpt)
+    model.load_state_dict(checkpoint['model_state'])
+
+    criterion = F.cross_entropy
+    regularizer = curves.l2_regularizer(args.wd)
+
+    cross_road = architecture.base(num_classes)
+    model.export_base_parameters(cross_road, 0)
+
+    tr_res = utils.test_extensive(loaders['train'], cross_road, criterion, regularizer)
+    te_res = utils.test_extensive(loaders['test'], cross_road, criterion, regularizer)
+
+    result = np.array([[tr_res["nll"], tr_res["loss"], tr_res["accuracy"] ], [te_res["nll"], te_res["loss"], te_res["accuracy"] ]])
+    
+    np.savez(os.path.join(args.dir, 'cross_road_results.npz'), result = result)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='DNN curve evaluation')
+    parser.add_argument('--dir', type=str, default='/tmp/eval', metavar='DIR',
+                        help='training directory (default: /tmp/eval)')
+
+    parser.add_argument('--num_points', type=int, default=61, metavar='N',
+                        help='number of points on the curve (default: 61)')
+
+    parser.add_argument('--dataset', type=str, default='CIFAR10', metavar='DATASET',
+                        help='dataset name (default: CIFAR10)')
+    parser.add_argument('--use_test', action='store_true',
+                        help='switches between validation and test set (default: validation)')
+    parser.add_argument('--transform', type=str, default='VGG', metavar='TRANSFORM',
+                        help='transform name (default: VGG)')
+    parser.add_argument('--data_path', type=str, default=None, metavar='PATH',
+                        help='path to datasets location (default: None)')
+    parser.add_argument('--batch_size', type=int, default=128, metavar='N',
+                        help='input batch size (default: 128)')
+    parser.add_argument('--num_workers', type=int, default=4, metavar='N',
+                        help='number of workers (default: 4)')
+
+    parser.add_argument('--model', type=str, default=None, metavar='MODEL',
+                        help='model name (default: None)')
+    parser.add_argument('--curve', type=str, default=None, metavar='CURVE',
+                        help='curve type to use (default: None)')
+    parser.add_argument('--num_bends', type=int, default=3, metavar='N',
+                        help='number of curve bends (default: 3)')
+
+    parser.add_argument('--ckpt', type=str, default=None, metavar='CKPT',
+                        help='checkpoint to eval (default: None)')
+
+    parser.add_argument('--wd', type=float, default=1e-4, metavar='WD',
+                        help='weight decay (default: 1e-4)')
+
+    args = parser.parse_args()
+    main(args)
+
+
